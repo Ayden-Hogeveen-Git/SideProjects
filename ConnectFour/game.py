a3a3e674d460d1deea769b9635b7564a6257e6b0
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2022-01-23 17:42:36 -0700

    Worked on the connect 4 game, added a font, started a title page, and worked on implementing a bitboard/gamestate board class

--- ConnectFour/game.py
+++ ConnectFour/game.py
@@ -1,5 +1,5 @@
 # game.py
-from loader import Colour
+from loader import Colour, Button
 import pygame
 pygame.init()
 
@@ -10,27 +10,71 @@ screen = pygame.display.set_mode((width, height))
 caption = "Connect 4"
 pygame.display.set_caption(caption)
 
+
 colour = Colour()
+font = pygame.font.Font("ConnectAssets/LandasansMedium-ALJ6m.otf", 32)
+
+
+class Board:
+    def __init__(self):
+        self.dimensionX = 8
+        self.dimensionY = 6
+
+        self.circle_width = width / self.dimensionX
+        self.buffer = width / 16
+
+        self.game_state = [
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""]
+        ]
+
+    def drawBoard(self):
+        for y in range(self.dimensionY):
+            for x in range(self.dimensionX):
+                if self.game_state[y][x] == "":
+                    pygame.draw.circle(screen, colour.white, (x * self.circle_width + self.buffer,
+                                       y * self.circle_width + self.buffer), self.circle_width / 4)
+                # 0 = Red Player
+                elif self.game_state[y][x] == "0":
+                    pygame.draw.circle(screen, colour.red, (x * self.circle_width + self.buffer,
+                                       y * self.circle_width + self.buffer), self.circle_width / 4)
+                # 0 = Yellow Player
+                elif self.game_state[y][x] == "1":
+                    pygame.draw.circle(screen, colour.yellow, (x * self.circle_width + self.buffer,
+                                       y * self.circle_width + self.buffer), self.circle_width / 4)
 
 
 class Game:
     def __init__(self):
         self.running = True
-
         self.scene = 1
+
+        self.playButton = Button(width / 2 - width / 16, height / 2 - height / 32, width / 8, height / 16,
+                                 colour.cyan, colour.blue, "Play Game")
+
+        self.board = Board()
 
     def draw_scenes(self):
         if self.scene == 1:
             screen.fill(colour.white)
+            self.playButton.drawButton(screen, font)
 
         elif self.scene == 2:
             screen.fill(colour.grey)
+
+            self.board.drawBoard()
+
         else:
             screen.fill(colour.black)
         pygame.display.update()
 
     def run(self):
         while self.running:
+            mouse_pos = pygame.mouse.get_pos()
             self.draw_scenes()
 
             for event in pygame.event.get():
@@ -39,5 +83,11 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.running = False
-                    if event.key == pygame.K_RETURN:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.playButton.mouseOn(mouse_pos):
                         self.scene += 1
+                if self.playButton.mouseOn(mouse_pos):
+                    pass
+
+
+
--- ConnectFour/loader.py
+++ ConnectFour/loader.py
@@ -1,4 +1,7 @@
 # loader.py
+import pygame
+
+
 class Colour:
     white = (255, 255, 255)
     light_grey = (100, 100, 100)
@@ -6,3 +9,42 @@ class Colour:
     dark_grey = (15, 15, 15)
     black = (0, 0, 0)
 
+    brown = (205, 175, 125)
+    beige = (255, 225, 200)
+
+    blue = (50, 50, 225)
+    cyan = (50, 200, 200)
+
+    red = (225, 50, 50)
+    yellow = (225, 225, 50)
+
+
+class Button:
+    def __init__(self, x, y, button_width, button_height, colour1, colour2, text=""):
+        self.colour = Colour()
+        self.x = x
+        self.y = y
+        self.width = button_width
+        self.height = button_height
+        self.text = text
+        self.colour1 = colour1
+        self.colour2 = colour2
+        self.border_width = 2
+
+    def drawButton(self, screen, font):
+        pygame.draw.rect(screen, self.colour1, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, self.colour2, (self.x, self.y, self.width, self.height), self.border_width)
+
+        text = font.render(self.text, True, self.colour.black)
+        screen.blit(text, (self.x + self.width / 16, self.y + self.height / 4))
+
+    def mouseOn(self, mouse_pos):
+        mouse_x = mouse_pos[0]
+        mouse_y = mouse_pos[1]
+
+        if self.x < mouse_x < self.x + self.width:
+            if self.y < mouse_y < self.y + self.height:
+                self.colour1 = self.colour2
+                self.border_width = 3
+                return True
+        return False
